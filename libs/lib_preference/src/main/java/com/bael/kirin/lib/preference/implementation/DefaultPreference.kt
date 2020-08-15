b58c81b61c8623a150ea7f2a0d6cc490f18ba8da
Erick Sumargo <ericksumargo@outlook.com>
2020-08-15 19:26:51 +0700

    changed

--- libs/lib_preference/src/main/java/com/bael/kirin/lib/preference/implementation/DefaultPreference.kt
+++ libs/lib_preference/src/main/java/com/bael/kirin/lib/preference/implementation/DefaultPreference.kt
@@ -1,9 +1,13 @@
 package com.bael.kirin.lib.preference.implementation
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
-import android.content.SharedPreferences.Editor
+import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV
+import androidx.security.crypto.EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+import androidx.security.crypto.MasterKey
+import androidx.security.crypto.MasterKeys.AES256_GCM_SPEC
 import com.bael.kirin.lib.logger.contract.Logger
 import com.bael.kirin.lib.preference.contract.Preference
 import com.bael.kirin.lib.resource.app.AppInfo
@@ -22,17 +26,31 @@ class DefaultPreference @Inject constructor(
 ) : Preference,
     Logger by logger {
     private val preferences: SharedPreferences by lazy {
-        context.getSharedPreferences(appInfo.name, MODE_PRIVATE)
-    }
+        val masterKey = MasterKey.Builder(context)
+            .setKeyGenParameterSpec(AES256_GCM_SPEC)
+            .build()
 
-    private val editor: Editor by lazy { preferences.edit() }
+        EncryptedSharedPreferences.create(
+            context,
+            appInfo.name,
+            masterKey,
+            AES256_SIV,
+            AES256_GCM
+        )
+    }
 
     override fun <T> read(key: String, defaultValue: T): T {
         return try {
             when (defaultValue) {
-                is String -> preferences.getString(key, defaultValue)
-                is Boolean -> preferences.getBoolean(key, defaultValue)
-                else -> throw UnsupportedOperationException()
+                is String -> {
+                    preferences.getString(key, defaultValue)
+                }
+                is Boolean -> {
+                    preferences.getBoolean(key, defaultValue)
+                }
+                else -> {
+                    throw UnsupportedOperationException()
+                }
             } as T
         } catch (cause: Exception) {
             log(cause)
@@ -43,9 +61,19 @@ class DefaultPreference @Inject constructor(
     override fun <T> write(key: String, value: T) {
         try {
             when (value) {
-                is String -> editor.putString(key, value).commit()
-                is Boolean -> editor.putBoolean(key, value).commit()
-                else -> throw UnsupportedOperationException()
+                is String -> {
+                    preferences.edit(commit = true) {
+                        putString(key, value)
+                    }
+                }
+                is Boolean -> {
+                    preferences.edit(commit = true) {
+                        putBoolean(key, value)
+                    }
+                }
+                else -> {
+                    throw UnsupportedOperationException()
+                }
             }
         } catch (cause: Exception) {
             log(cause)
